c930902eeead4b220cbf2d1215b1572761098123
horsefeedapples <54728068+horsefeedapples@users.noreply.github.com>
2020-12-14 23:30:41 -0500

    Added Login/Signup

---
+++ lib/screens/bottom_nav_bar.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class BottomNavBar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text("We have logged in"),
+    );
+  }
+}
