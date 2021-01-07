ff53f717fd4845f69a5f07f505752de80e701e11
VGoma23 <72355741+VGoma23@users.noreply.github.com>
2021-01-07 17:52:11 -0500

    Adding Comments (1)

--- lib/widgets/profile/interest_item.dart
+++ lib/widgets/profile/interest_item.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 class InterestItem extends StatelessWidget {
   final String interest;
   InterestItem(this.interest);
+  // Constant variable interest and constuctor. Each Interest Item consists of a a single string.
 
   @override
   Widget build(BuildContext context) {
@@ -12,7 +13,7 @@ class InterestItem extends StatelessWidget {
         Container(
           margin: EdgeInsets.fromLTRB(15, 15, 15, 0),
           child: Text(
-            interest,
+            interest, // interest text.
             style: TextStyle(fontSize: 10),
           ),
         ),
