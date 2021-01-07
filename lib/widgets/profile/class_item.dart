6ee59a42c3799a1b7ca571c8f3785154f45dafc9
VGoma23 <72355741+VGoma23@users.noreply.github.com>
2021-01-07 17:49:23 -0500

    Adding Comments (1)

--- lib/widgets/profile/class_item.dart
+++ lib/widgets/profile/class_item.dart
@@ -7,27 +7,27 @@ class ClassItem extends StatelessWidget {
   ClassItem(this.cls, this.grade, this.year);
   @override
   Widget build(BuildContext context) {
-    return Row(
+    return Row( // Each row consists of three categories, a class, grade, and year.
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
         Container(
           margin: EdgeInsets.fromLTRB(15, 15, 15, 0),
           child: Text(
-            cls,
+            cls, // class text
             style: TextStyle(fontSize: 10),
           ),
         ),
         Container(
           margin: EdgeInsets.fromLTRB(15, 15, 15, 0),
           child: Text(
-            grade,
+            grade, // grade text
             style: TextStyle(fontSize: 10),
           ),
         ),
         Container(
           margin: EdgeInsets.fromLTRB(15, 15, 15, 0),
           child: Text(
-            year,
+            year, // year text
             style: TextStyle(fontSize: 10),
           ),
         ),
