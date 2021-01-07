ea4afd9677241e9ed86e071679bc7f1b99f663b7
VGoma23 <72355741+VGoma23@users.noreply.github.com>
2021-01-07 17:50:24 -0500

    Update experience_item.dart

--- lib/widgets/profile/experience_item.dart
+++ lib/widgets/profile/experience_item.dart
@@ -4,6 +4,7 @@ class ExperienceItem extends StatelessWidget {
   final String experience;
   final String year;
   ExperienceItem(this.experience, this.year);
+  // Constant variables and constructor. Each Experience Item contains the experience title and year.
 
   @override
   Widget build(BuildContext context) {
@@ -13,14 +14,14 @@ class ExperienceItem extends StatelessWidget {
         Container(
           margin: EdgeInsets.fromLTRB(15, 15, 15, 0),
           child: Text(
-            experience,
+            experience, // Experience title text.
             style: TextStyle(fontSize: 10),
           ),
         ),
         Container(
           margin: EdgeInsets.fromLTRB(15, 15, 15, 0),
           child: Text(
-            year,
+            year, // Year text
             style: TextStyle(fontSize: 10),
           ),
         ),
