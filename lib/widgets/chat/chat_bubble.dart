4d3071328cc16d761394dd0f93c23b1f48914bae
VGoma23 <72355741+VGoma23@users.noreply.github.com>
2021-01-09 22:59:24 -0500

    Adding Comments (1)

--- lib/widgets/chat/chat_bubble.dart
+++ lib/widgets/chat/chat_bubble.dart
@@ -13,21 +13,21 @@ class ChatBubble extends StatelessWidget {
   Widget build(BuildContext context) {
     return Stack(
       children: [
-        Row(
+        Row( // A chat bubble is a row with color and spacing based on which user sent the message.
           mainAxisAlignment:
               isMe ? MainAxisAlignment.end : MainAxisAlignment.start,
           children: [
             Container(
               decoration: BoxDecoration(
-                color: isMe ? Colors.grey[300] : Theme.of(context).accentColor,
+                color: !isMe ? Colors.grey[300] : Theme.of(context).accentColor, // Blue if sent by user, grey if sent by receiving user.
                 borderRadius: BorderRadius.only(
                   topLeft: Radius.circular(12),
                   topRight: Radius.circular(12),
                   bottomLeft: !isMe ? Radius.circular(0) : Radius.circular(12),
-                  bottomRight: isMe ? Radius.circular(0) : Radius.circular(12),
+                  bottomRight: isMe ? Radius.circular(0) : Radius.circular(12), // Shape of bubble determined by which side it's on, which depends on who sent the message.
                 ),
               ),
-              width: 140,
+              width: 200,
               padding: EdgeInsets.symmetric(
                 vertical: 10,
                 horizontal: 16,
@@ -38,16 +38,16 @@ class ChatBubble extends StatelessWidget {
               ),
               child: Column(
                 crossAxisAlignment:
-                    isMe ? CrossAxisAlignment.end : CrossAxisAlignment.start,
+                    isMe ? CrossAxisAlignment.end : CrossAxisAlignment.start,  // Received messages are on the left, sent messages are on the right.
                 children: [
                   Text(
-                    message,
+                    message, // Actual message.
                     style: TextStyle(
-                      color: isMe
+                      color: !isMe
                           ? Colors.black
-                          : Theme.of(context).accentTextTheme.headline1.color,
+                          : Theme.of(context).accentTextTheme.headline1.color, // Text color to contrast with the bubble color.
                     ),
-                    textAlign: isMe ? TextAlign.end : TextAlign.start,
+                    textAlign: isMe ? TextAlign.end : TextAlign.start, // Text align, left and right justification.
                   ),
                 ],
               ),
